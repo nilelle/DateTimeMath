03868b340153700a18d2bac01ac15bf9a6bdc49b
Bora Kaya <bora.587@hotmail.com>
2022-03-07 23:28:22 +0300

    Add project files.

---
+++ DateTimeMath/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeMath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+            Console.WriteLine(DateTime.Now.Millisecond);
+            Console.WriteLine(DateTime.Now.Month);
+
+            Console.WriteLine(Math.Sin(1));
+            Console.WriteLine(Math.Exp(4));
+            Console.WriteLine(Math.Pow(2,4));
+            Console.WriteLine(Math.Sqrt(36));
+
+            Console.ReadKey();  
+        }
+    }
+}
